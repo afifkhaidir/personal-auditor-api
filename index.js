352ac72eb363a4a1a8735f71196ab59daf2f4360
Khaidir Afif <afif.khaidir@gmail.com>
2017-03-23 19:24:24 +0700

    Create PUT and DELETE method

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ router.use((req, res, next) => {
 /* routes for /api/expense */
 router.route('/expense')
   .get((req, res) => {
-    // retrieve all expenses
     Expense.find().exec()
       .then(expenses => res.json(expenses))
       .catch(err => res.json({ error: err }))
@@ -65,6 +64,29 @@ router.route('/expense')
 
     newExpense.save()
       .then(expense => res.json({ message: 'Expense added', expense: expense }))
+      .catch(err => res.json({ error: err }))
+  })
+
+router.route('/expense/:expense_id')
+  .get((req, res) => {
+    Expense.findById(req.params.expense_id).exec()
+      .then(expense => res.json(expense))
+      .catch(err => res.json({ error: err }))
+  })
+  .put((req, res) => {
+    Expense.findById(req.params.expense_id).exec()
+      .then(expense => {
+        expense.name = req.body.name
+        expense.amount = req.body.amount
+
+        return expense.save()
+      })
+      .then(expense => res.json({ success: true, message: 'Updated!', expense: expense }))
+      .catch(err => res.json({ success: false, error: err }))
+  })
+  .delete((req, res) => {
+    Expense.remove({ _id: req.params.expense_id }).exec()
+      .then(status => res.json({ message: 'Deleted!', status: status }))
       .catch(err => res.json({ error: err }))
   })
 
